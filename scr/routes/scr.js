3a61f2b34d4daeaac50735d813e47156968bcfbf
Sujith John(johns) <johns@wfu.edu>
2019-01-14 16:11:40 -0500

    added

---
+++ scr/routes/scr.js
@@ -0,0 +1,33 @@
+
+var express = require('express');
+var router = express.Router();
+
+router.get('/', function(req, res) {
+  var cheerio = require("cheerio");
+  var axios = require("axios");
+  axios.get("https://www.nytimes.com/section/todayspaper").then(function(response) {
+    var $ = cheerio.load(response.data);
+    var results = [];
+    $(".css-141drxa").each(function(i, element) {
+      var title = $(element).text();
+      var link = "https://www.nytimes.com" + $(element).children().attr("href");
+      results.push({
+        title: title,
+        link: link
+      });
+    });
+    console.log(results.length);
+    for (var i=0; i<results.length;i++){
+      console.log("Title: " + results[i].title + "\n\r" + "Links: " + results[i].link)
+    }
+    // res.send(results);
+    res.render('scrape', {
+      link: results[0].link,
+      titles: results[0].title
+    
+    });
+  });
+  
+});
+
+module.exports = router;
